330f4e874a04ad736e0efc4e457a8798b699d60d
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-11-01 18:12:09 -0400

    tiadd more properties to ticketSchema

--- models/flight.js
+++ models/flight.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const ticketSchema = new Schema({
-  seat: String,
+  seat: {type: String, match: /[A-F][1-9]\d?/},
   price: {type: Number, min: 0}
 }, {
   timestamps: true
@@ -14,7 +14,8 @@ const flightSchema = new Schema({
   airline: {type: String, enum: ["American", "Southwest", "United"]},
   airport: {type: String, enum: ["AUS", "DFW", "DEN", "LAX", "SAN", "BOS"], default: 'DEN'},
   flightNo: {type: Number, min: 10, max: 9999},
-  departs: {type: Date, default: new Date().setFullYear(new Date().getFullYear() + 1)}
+  departs: {type: Date, default: new Date().setFullYear(new Date().getFullYear() + 1)},
+  tickets: [ticketSchema]
 }, {
   timestamps: true
 })
